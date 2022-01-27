56aa4b252a6696cb0a81d4574b6902ac416f3660
samuel <samuelcontatodev@gmail.com>
2022-01-26 21:15:26 -0300

    crud node js

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express')
+
+const app = express()
+
+//defifindo que ele vai usar json
+app.use(express.json())
+
+const cursos = ['Lógica', 'SQL', 'Node', 'HTML']
+
+//retorna apenas um curso pelo index
+app.get('/cursos/:index', (req, res) => {
+    const {index} = req.params
+
+    return res.json(cursos[index])
+})
+
+//retorna todos os cursos
+app.get('/cursos', (req, res) => {
+    return res.json(cursos)
+})
+
+//criar um novo cursos
+app.post('/cursos', (req, res) => {
+    const {name} = req.body
+    cursos.push(name)
+
+    return res.json(cursos)
+})
+
+//atualizar cursos
+app.put('/cursos/:index', (req, res) => {
+    const {index} = req.params
+    const {name} = req.body
+
+    cursos[index] = name
+
+    return res.json(cursos)
+})
+
+//deletar um curso
+app.delete('/cursos/:index', (req, res) => {
+    const {index} = req.params
+
+    cursos.splice(index, 1)
+
+    return res.json({message: 'O curso foi deletado'})
+})
+
+app.listen(3000, () => {
+    console.log('Rodando...')
+})
